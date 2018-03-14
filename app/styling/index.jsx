2d980d82d90f029e8a8c5bbb62661a100d631b23
Elliott Thompson <elliott@moo.com>
2018-03-14 20:09:22 +0000

    added fonts and more styling

--- app/styling/index.jsx
+++ app/styling/index.jsx
@@ -1,4 +1,6 @@
 import styled, { injectGlobal,css } from 'styled-components';
+import Montserrat from 'fonts/Montserrat/Montserrat-Bold.ttf';
+import Merriweather from 'fonts/Merriweather/Merriweather-Regular.ttf';
 
 export const media = {
     phone: (...args) => css`
@@ -14,12 +16,43 @@ export const media = {
 };
 
 injectGlobal`
+    @font-face {
+        font-family: TitleFont;
+        src: url('${Montserrat}') format('opentype');
+    }
+    @font-face {
+        font-family: ParagraphFont;
+        src: url('${Merriweather}') format('opentype');
+    }
+    html, body {
+        margin: 0px;
+    }
+    h1, h2, h3, h4, h5 {
+        font-family: TitleFont;
+    }
+    p, li, a {
+        font-family: ParagraphFont;
+        font-size: 14px;
+    }
+`;
+
+export const Banner = styled.div`
+    background-color: #43A047;
 `;
 
 export const NavContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    color: white;
+    a {
+        text-decoration: none;
+        color: white;
+        transition: all 0.5s;
+    }
+    a:hover {
+        opacity: 0.5;
+    }
 `;
 
 export const Nav = styled.div`
@@ -27,15 +60,20 @@ export const Nav = styled.div`
 `;
 
 export const NavItem = styled.a`
-    margin-left: 1rem;
+    margin-left: 2rem;
 `;
 
 export const Img = styled.img`
     float:right;
     max-width: 470px;
+    padding-left: 2rem;
 `;
 
 export const Container = styled.div`
     max-width: 960px;
     margin: 0 auto;
+`;
+
+export const PageContent = styled.div`
+    margin: 2rem 0;
 `;
