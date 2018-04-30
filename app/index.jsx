eb8a73677eb0cf9ecd089ef4ce3fbc80d80a6425
Elliott Thompson <elliott@moo.com>
2018-04-30 21:52:58 +0100

    refactor directory

--- app/index.jsx
+++ app/index.jsx
@@ -37,93 +37,80 @@ class App extends Component {
     }
 }
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <S.Img src='https://i.imgur.com/KypHy4z.jpg'/>
-                <p>I am a PhD candidate in Demography at the <a href='http://www.lse.ac.uk/'>London School of Economics and Political Science (LSE)</a>. Before my PhD, I completed a BSc in Economics at the <a href='https://www.essex.ac.uk/'>University of Essex</a> and MSc’s in Population and Development and Social Research Methods at the LSE. In 2017, I conducted an Economic and Social Research Council (ESRC) funded research visit at the <a href='http://health.snu.ac.kr/en'>Graduate School of Public Health</a>. at Seoul National University in South Korea</p>
-                <p>My <a href='#/research'>research</a> is broadly on individual ageing, with a focus on the interactions between the societal context in which older people live and their participation in socially productive roles. For my PhD thesis, I am using longitudinal survey data to analyse intergenerational transfers of support and participation in paid work and family care among middle-aged and older people in Italy and South Korea.</p>
-                <p>My primary methods of analysis include multilevel models, multivariate analysis and causal inference methods. My <a href='#/teaching'>teaching</a> at the LSE also reflects my interest in quantitative social science research methods.</p>
-                <p>I am a member of the Ageing, Lifecourse and Population Health Analysis <a href='http://www.lse.ac.uk/social-policy/research/Research-clusters/ALPHA'>(ALPHA)</a> group.</p>
-                <p>For more information, please <a href='mailto:g.floridi@lse.ac.uk?Subject=Hello'>get in touch</a> or download a copy of <a href='https://drive.google.com/file/d/1UJJkayEwCxR5YIRV2G_Tt-hnwmv2Bq8O/view?usp=sharing'>my CV</a>.</p>
-            </div>
-        );
-    }
+class ComponentWithData extends Component {
+  constructor(props) {
+    super(props);
+    this.getData = this.getData.bind(this);
+    this.createMarkup = this.createMarkup.bind(this);
+    this.renderMain = this.renderMain.bind(this);
+    this.state = { data: null };
+  }
+  getData(id) {
+    const url = `https://www.googleapis.com/drive/v3/files/${id}?key=AIzaSyAxR92_GtCJz2K7IQANqGpaKellvj1W3cE&alt=media`;
+    fetch(url)
+      .then(response => response.blob())
+      .then(response => {
+        var reader = new FileReader();
+        reader.onload = () => {
+          this.setState({data: reader.result});
+        };
+        reader.readAsText(response);
+      });
+  }
+  createMarkup() {
+    return { __html: this.state.data };
+  }
+  render() {
+    if (!this.state.data) return (<div></div>);
+    return this.renderMain();
+  }
 }
 
-class Research extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Publications</h2>
-                <p>Floridi, G. (2018): Social policies and intergenerational support in Italy and South Korea, Contemporary Social Science, published online: 12/03/2018. <a href='https://www.tandfonline.com/doi/full/10.1080/21582041.2018.1448942'>DOI: 10.1080/21582041.2018.1448942</a></p>
-                <h2>Work in progress</h2>
-                <p>Floridi, G.: Family matters: Comparing the predictors of paid work and informal caregiving after age 50 in Italy and South Korea. In preparation.</p>
-                <p>Floridi, G. & Lauderdale, B.: Self-discovery method for supervised measurement: An application to the concept of ‘productive ageing’. In preparation</p>
-                <h2>Selected conference presentations</h2>
-                <h4>“Family matters: Comparing the predictors of paid work and informal caregiving after age 50 in Italy and South Korea”</h4>
-                <ul>
-                    <li>Forthcoming: European Population Conference, 6th – 9th June 2018, Brussels, Belgium</li>
-                    <li>Population Association of America Annual Conference, 26th – 28th April 2018, Denver, United States</li>
-                </ul>
-                <h4>“Working and informal caregiving among older Italian and South Korean parents”</h4>
-                <ul>
-                    <li>International Population Conference, International Union for the Scientific Study of Population, 29th October – 4th November 2017, Cape Town, South Africa</li>
-                </ul>
-                <h4>“Social policies and intergenerational support in Italy and South Korea”</h4>
-                <ul>
-                    <li>International Population Conference, International Union for the Scientific Study of Population, 29th October – 4th November 2017, Cape Town, South Africa</li>
-                    <li>British Society for Population Studies Annual Meeting, 6th– 8th September 2017, Liverpool, UK</li>
-                    <li>Korean Population Association Biennial Meeting, 21st July 2017, Seoul, South Korea</li>
-                </ul>
-            </div>
-        );
-    }
+class Home extends ComponentWithData {
+  componentWillMount() {
+    this.getData("1acgy4aJ80z70Qa4G0XsNAJUXf6AAOWXv");
+  }
+  renderMain() {
+    return (
+        <div>
+          <S.Img src='https://i.imgur.com/KypHy4z.jpg'/>
+          <div dangerouslySetInnerHTML={this.createMarkup()}></div>
+        </div>
+    );
+  }
 }
 
-class Teaching extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Guest lecturer</h3>
-                <ul>
-                    <li>Intermediate quantitative analysis (Postgraduate level) @ <a href='http://www.lse.ac.uk/methodology'>Department of Methodology</a>, LSE</li>
-                </ul>
-                <h3>Graduate teaching assistant</h3>
-                <ul>
-                    <li>Causal inference for observational and experimental studies (Postgraduate level) @ <a href='http://www.lse.ac.uk/methodology'>Department of Methodology</a>, LSE</li>
-                    <li>Research methods for Social Policy (Undergraduate level) @ <a href='http://www.lse.ac.uk/social-policy'>Department of Social Policy</a>, LSE</li>
-                    <li>Intermediate quantitative analysis (Postgraduate level) @ <a href='http://www.lse.ac.uk/methodology'>Department of Methodology</a>, LSE</li>
-                </ul>
-                <h3>Project officer (advice and support with dissertations)</h3>
-                <ul>
-                    <li>Executive Masters in Global Management dissertation (Postgraduate level) @ <a href='http://www.lse.ac.uk/management'>Department of Management</a>, LSE</li>
-                </ul>
-            </div>
-        );
-    }
+class Research extends ComponentWithData {
+  componentWillMount() {
+    this.getData("15bBeDOieDGXmm4O5vrTy18YuJEOHsQvt");
+  }
+  renderMain() {
+    return (
+      <div dangerouslySetInnerHTML={this.createMarkup()}></div>
+    );
+  }
 }
 
-class Contact extends Component {
-    render() {
-        return (
-            <div>
-                <h4>Address:</h4>
-                <p>
-                    Department of Social Policy,<br/>
-                    2nd Floor, Old Building,<br/>
-                    London School of Economics and Political Science,<br/>
-                    Houghton Street,<br/>
-                    London WC2A 2AE
-                </p>
-                <h4>Email:</h4>
-                <p>g.floridi@lse.ac.uk</p>
-                <h4>Twitter:</h4>
-                <p>@ginevra_floridi</p>
-            </div>
-        );
-    }
+class Teaching extends ComponentWithData {
+  componentWillMount() {
+    this.getData("14fDsHAz2wr0JAVSMz8zsktcoNRYHIibw");
+  }
+  renderMain() {
+    return (
+      <div dangerouslySetInnerHTML={this.createMarkup()}></div>
+    );
+  }
+}
+
+class Contact extends ComponentWithData {
+  componentWillMount() {
+    this.getData("1aFVsP3yMmT09x-1k38ppSODSWZ28L0Wi");
+  }
+  renderMain() {
+    return (
+      <div dangerouslySetInnerHTML={this.createMarkup()}></div>
+    );
+  }
 }
 
 render(
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express')
+const request = require('request')
 const app = express()
+
+const serverConfig = require('./server_config.json');
 
 app.use(express.static('dist'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/dist/index.html'))
+
+const ids = {
+  homepage: "1acgy4aJ80z70Qa4G0XsNAJUXf6AAOWXv",
+  research: "15bBeDOieDGXmm4O5vrTy18YuJEOHsQvt",
+  teaching: "14fDsHAz2wr0JAVSMz8zsktcoNRYHIibw",
+  contact: "1aFVsP3yMmT09x-1k38ppSODSWZ28L0Wi"
+};
+
+app.get('/blob/:id', (req, res) => {
+  let url = `https://www.googleapis.com/drive/v3/files/${ids[req.params.id]}?key=${serverConfig.apiKey}&alt=media`;
+  request({
+    url: url,
+    json: false
+  }, function (error, response, body) {
+    res.send(body);
+  });
+})
 
 app.listen(1337, () => console.log('Ginevras website is running on port 1337!'))
